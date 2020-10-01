1fd01de9217353e8a86451d3b613c6785fd88739
adantop <>
2020-10-01 21:43:43 +0000

    reworked to containerize

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+cd frontend
+docker image build -t todo_frontend:0.0.1 .
+docker run --name frontend -d -p 5001:5001 todo_frontend:0.0.1
+
+
+cd ../backend
+docker image build -t todo_backend:0.0.1 .
+docker run --name backend -d -p 5000:5000 todo_backend:0.0.1
--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, static_url_path='', static_folder='../frontend', template_folder='../frontend')
+app = Flask(__name__, static_url_path='', static_folder='www', template_folder='www')
 
 
 @app.route('/', methods=['GET'])
